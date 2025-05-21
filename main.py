99e68c1e422ed46ead808d8402d619d6ab03b133
Luiz Carlos de Azevedo Santana <Luiz Carlos de Azevedo Santana>
2025-05-21 15:03:32 -0300

    feature: added hash generation for each block

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class PrivateBlock(TypedDict):
     content: str
     author: str
     commitedKey: str
+    counter: int
 
 class MessageContent(TypedDict):
     content: str
@@ -19,13 +20,52 @@ class MessageContent(TypedDict):
 
 class CommitArgs(TypedDict):
     commitedKey: str
+    message: MessageContent
+
+class CommitArgsProcessed(TypedDict):
+    commitedKey: str
     message: str
+
+class HashArgs(TypedDict):
+    previousHash: str
+    counter: int
+    commitedMessage: str
 
 def file_reader(filepath: str):
     with open(filepath) as f:
         json_object = json.load(f)
 
     return json_object
+
+def generate_auditable_hash(public_block: AuditableBlock, private_block: PrivateBlock):
+    message_content: MessageContent = {
+        'content': private_block['content'],
+        'author': private_block['author']
+    }
+    commit_message_args: CommitArgs = {
+        'commitedKey': private_block['commitedKey'],
+        'message': message_content
+    }
+    commited_message = commit_function(commit_message_args)
+
+    hash_args: HashArgs = {
+        'previousHash': public_block['previousHash'],
+        'counter': public_block['counter'],
+        'commitedMessage': commited_message
+    }
+    serialized_data = json.dumps(hash_args, ensure_ascii=False, separators=(',', ':'))
+    hash =  sha256(serialized_data.encode('utf-8')).hexdigest()
+
+    return hash
+
+def commit_function(args: CommitArgs):
+    processed_message_content = json.dumps(args['message'], ensure_ascii=False, separators=(',', ':'))
+    commit_args: CommitArgsProcessed = {
+        'commitedKey': args['commitedKey'],
+        'message': processed_message_content
+    }
+    serialized_data = json.dumps(commit_args, ensure_ascii=False, separators=(',', ':'))
+    return sha256(serialized_data.encode('utf-8')).hexdigest()
 
 def main(public_filepath: str, private_filepath: str):
     public_content = file_reader(public_filepath)
@@ -36,26 +76,21 @@ def main(public_filepath: str, private_filepath: str):
     initial_commited_key: str = private_content['initialCommitedKey']
     chat_content: List[PrivateBlock] = private_content['logMessages']
 
-    print("chat_content is: ", chat_content[1])
+    print(f'Starting audition on block listed in {private_filepath}')
+    print(f'Reference file: {public_filepath}')
 
-    test_message: MessageContent = {
-        "content": chat_content[1]["content"],
-        "author": chat_content[1]["author"]
-    }
-    print("commit object: ", {
-        "commitedKey": chat_content[1]["commitedKey"],
-        "message": json.dumps(test_message, ensure_ascii=False, separators=(',', ':'))
-    })
-    first_commit = commit_function({
-        "commitedKey": chat_content[1]["commitedKey"],
-        "message": json.dumps(test_message, ensure_ascii=False, separators=(',', ':'))
-    })
-    print(first_commit)
+    for private_block in chat_content:
+        counter = private_block['counter']
+        public_block = hashchain[counter-1]
+        generated_hash = generate_auditable_hash(public_block, private_block)
 
-def commit_function(args: CommitArgs):
-    serialized_data = json.dumps(args, ensure_ascii=False, separators=(',', ':'))
-    return sha256(serialized_data.encode('utf-8')).hexdigest()
+        if (generated_hash != public_block['hash']):
+            print(f'Private Block {private_block} hash did not match the one from Public Block {public_block}')
+            print(f'Public Block hash: {public_block["hash"]}')
+            print(f'Private Block hash: {generated_hash}')
 
-public_filepath = "./test_files/total_conversation/public_logs_2025-05-21.json"
-private_filepath = "./test_files/total_conversation/private_logs_2025-05-21.json"
+    print("Audition completed.")
+
+public_filepath = "./test_files/partial_conversation/public_logs_2025-05-21.json"
+private_filepath = "./test_files/partial_conversation/private_logs_2025-05-21.json"
 main(public_filepath, private_filepath)
